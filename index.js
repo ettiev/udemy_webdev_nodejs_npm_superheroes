f59fe9e7ebf47bf1b3c53e7f31a5fa3e7354bd2c
ettiev <126219891+ettiev@users.noreply.github.com>
2023-11-21 09:05:04 +0200

    Udemy - Intro to NodeJS and NPM
    Finished Udemy Project - Intro to NodeJs and NPM

---
+++ index.js
@@ -0,0 +1,12 @@
+var superheroes = require("superheroes");
+
+var mySuperHeroName = superheroes.random();
+
+console.log("Superhero name: "+ mySuperHeroName);
+
+
+const supervillains = require("supervillains");
+
+var mySupervillianName = supervillains.random();
+
+console.log("Villain Name: " + mySupervillianName);
